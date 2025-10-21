453c73b0bf4ec7b0013917b8db8b21873288af16
haris-demo <haris.awann25@gmail.com>
2025-10-21 22:27:28 +0500

    initial

--- src/Cover.jsx
+++ src/Cover.jsx
@@ -1,15 +1,26 @@
-import styles from './css/Cover.module.css'
-
-
 const Cover = () => {
-    return (
-        <div>
-            <div className="mt-[90px] w-full sm:h-[400px] h-[300px] rounded-[20px] bg-[center] flex justify-center items-center text-[100px] text-white bg-[linear-gradient(rgba(0,0,0,0.8),rgba(0,0,0,0.8)),url('https://cdn.shopify.com/s/files/1/0627/5517/files/02-26-20_Aidan_264883.jpg?v=1603213851')] bg-center bg-cover "><p className="sm:text-center text-[80px] " >HM Store</p></div>
-            <div className="h-[60px] bg-[#dedbd2] border-2  border-[#d5bdaf] rounded-[5px] flex m-[5px]  justify-center content-center overflow-hidden ">                
-                <p className={`sm:items-center sm:font-serif sm:font-light sm:tracking[5px] text-lg text-nowrap font-serif content-center m-0 font-bold tracking-[5px] text-2xl ${styles.slide}`}>WE BELIEVE IN QUALITY</p>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div>
+      {/* Hero Section */}
+      <div
+        className="mt-[90px] w-full h-[300px] sm:h-[400px] rounded-[20px] flex justify-center items-center text-white bg-center bg-cover"
+        style={{
+          backgroundImage: `linear-gradient(rgba(0,0,0,0.8), rgba(0,0,0,0.8)), url('https://cdn.shopify.com/s/files/1/0627/5517/files/02-26-20_Aidan_264883.jpg?v=1603213851')`,
+        }}
+      >
+        <p className="text-[80px] sm:text-center sm:text-[100px] font-bold">
+          HM Store
+        </p>
+      </div>
 
-export default Cover
+      {/* Banner/Slide Text */}
+      <div className="h-[60px] bg-[#dedbd2] border-2 border-[#d5bdaf] rounded-[5px] flex justify-center items-center overflow-hidden m-[5px]">
+        <p className="text-2xl sm:text-lg font-serif font-bold tracking-[5px] animate-marquee whitespace-nowrap">
+          WE BELIEVE IN QUALITY
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Cover;
